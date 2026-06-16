ed0c0d1e9a5f0f08fef296ef031ae5b027c053f1
sarthak singh chauhan <scchessking@gmail.com>
2026-06-16 16:04:26 +0530

    Add LeetCode 567 Permutation in String solution

---
+++ 0567_Permutation_in_String.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LeetCode 567: Permutation in String
+// Approach: Frequency Count + Brute Force Window Checking
+// Time Complexity: O(n * m)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isFreqSame(int freq1[], int freq2[]) {
+        for(int i = 0; i < 26; i++) {
+            if(freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] = {0};
+
+        for(int i = 0; i < s1.length(); i++) {
+            freq[s1[i] - 'a']++;
+        }
+
+        int windowSize = s1.length();
+
+        for(int i = 0; i < s2.length(); i++) {
+            int windowIdx = 0, idx = i;
+            int windowFreq[26] = {0};
+
+            while(windowIdx < windowSize && idx < s2.length()) {
+                windowFreq[s2[idx] - 'a']++;
+                windowIdx++;
+                idx++;
+            }
+
+            if(isFreqSame(freq, windowFreq)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
